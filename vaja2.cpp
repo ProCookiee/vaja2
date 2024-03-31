303560fe9a938797fefbad981821952234e60b86
Anej <anej.sukic@student.um.si>
2024-03-31 16:45:01 +0200

    8 bit binary

--- vaja2.cpp
+++ vaja2.cpp
@@ -4,6 +4,7 @@
 
 using namespace std;
 
+//Prebere števila iz datoteke vhod.txt in jih shrani v vektor
 void preberi(std::vector<int> &v) {
     ifstream vhod("vhod.txt");
     int x;
@@ -13,6 +14,7 @@ void preberi(std::vector<int> &v) {
     vhod.close();
 }
 
+//Pretvori decimalno število v binarno število
 string decToBin(int x) {
     string bin = "";
     while(x > 0) {
@@ -22,12 +24,26 @@ string decToBin(int x) {
     return bin;
 }
 
+//Naredi vektor binarnih števil in doda 0 na začetek da so vsa števila dolga 8 mest ker je 8bit
+vector<string> binarna(vector<int> numbers) {
+    vector<string> bin;
+    for(int i = 0; i < numbers.size(); i++) {
+        string b = decToBin(numbers[i]);
+        while(b.length() < 8) {
+            b = "0" + b;
+        }
+        bin.push_back(b);
+    }
+    return bin;
+}
+
 int main() {
-    std::vector<int> v;
-    preberi(v);
+    vector<int> numbers;
+    preberi(numbers);
+    vector<string> bin = binarna(numbers);
     ofstream izhod("izhod.txt");
-    for(int i = 0; i < v.size(); i++) {
-        izhod << decToBin(v[i]) << endl;
+    for(int i = 0; i < bin.size(); i++) {
+        izhod << bin[i] << endl;
     }
     izhod.close();
     return 0;
